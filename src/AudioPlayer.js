2a6e853553f2b5e837cb9b61333f617e1ab4af43
linnn-7 <linbinjulie@gmail.com>
2024-03-11 01:56:36 +0800

    spotify web playback sdk

--- src/AudioPlayer.js
+++ src/AudioPlayer.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { tracks } from './tracks';
+import WebPlayback from './WebPlayback'
+import Login from './login'
 import DisplayTrack from './DisplayTrack';
 
 const AudioPlayer = () => {
     const [currentTrack, setCurrentTrack] = useState(tracks[0]);
-    return (
-      <div className="audio-player">
-        <div className="inner">
-          <DisplayTrack currentTrack={currentTrack} />
-        </div>
-      </div>
-    );
-  };
+    const [token, setToken] = useState('');
+
+  useEffect(() => {
+
+    async function getToken() {
+      console.log("start fetching response")
+      const response = await fetch('/auth/token');
+      console.log(response)
+      const json = await response.json();
+      setToken(json.access_token);
+    }
+
+    getToken();
+
+  }, []);
+
+  return (
+    <>
+        { (token === '') ? <Login/> : <WebPlayback token={token} /> }
+    </>
+  );
+};
 export default AudioPlayer;
